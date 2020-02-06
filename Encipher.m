1d1d20e478cb19f43669d51ba3d59058c28b59e0
unknown <435869851@qq.com>
2020-02-06 16:36:54 +0800

    increase the encryption function

--- Encipher.m
+++ Encipher.m
@@ -1,6 +1,6 @@
 %file: Encipher.m
 %to encipher the image after the saving space progress
-%origin: the original image
+%origin: the image after saving space progress
 %key: the sub key to generate the stream key to encipher
 %using  key expansion function
 function EnImage = Encipher( origin , key )
